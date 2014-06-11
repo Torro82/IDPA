7f61e38b54a17705d809c9f446488cbcf080b0b2
Torro82 <ava.navi.tar@gmail.com>
2014-06-11 10:56:38 +0200

    Merge branch 'master' of https://github.com/Torro82/IDPA
    Conflicts:
    	src/com/idpa/tasktransmitter2/ShowTasksActivity.java

--- src/com/idpa/tasktransmitter2/ShowTasksActivity.java
+++ src/com/idpa/tasktransmitter2/ShowTasksActivity.java
@@ -18,13 +18,13 @@ public class ShowTasksActivity extends Activity {
 		int size = MainActivity.Task.size();
 		for (int i = 0; i < size; i++) {
 			int j = i+1;
-			list.setText(list.getText()+Integer.toString(j)+". " + HelperSharedPreferences.getSharedPreferencesString(getApplicationContext(),"index_date_"+i, "Nox exists")+" / "+ HelperSharedPreferences.getSharedPreferencesString(getApplicationContext(),"index_subject_"+i, "Nox exists")+"\n"+ HelperSharedPreferences.getSharedPreferencesString(getApplicationContext(),"index_task_"+i, "Nox exists")+"\n"+"\n");
+			list.setText(list.getText()+Integer.toString(j)+". " + HelperSharedPreferences.getSharedPreferencesString(getApplicationContext(),"index_date_"+i, "Not exists")+" / "+ HelperSharedPreferences.getSharedPreferencesString(getApplicationContext(),"index_subject_"+i, "Not exists")+"\n"+ HelperSharedPreferences.getSharedPreferencesString(getApplicationContext(),"index_task_"+i, "Not exists")+"\n"+"\n");
 		}
 		if(list.getText().equals("")) {
 			Toast.makeText(getApplicationContext(), "You don't have any tasks...", Toast.LENGTH_SHORT).show();
 		}
-		
 	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
