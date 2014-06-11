18930ec9e9eaed62f11cff8a5a8a4e9dbccb6e49
Torro82 <ava.navi.tar@gmail.com>
2014-06-11 10:52:54 +0200

    Update ShowTasksActivity.java

--- src/com/idpa/tasktransmitter2/ShowTasksActivity.java
+++ src/com/idpa/tasktransmitter2/ShowTasksActivity.java
@@ -25,21 +25,6 @@ public class ShowTasksActivity extends Activity {
 		}
 	}
 	
-	protected void onResume(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_show_tasks);
-		
-		TextView list = (TextView) findViewById(R.id.tasks);
-		
-		int size = MainActivity.Task.size();
-		for (int i = 0; i < size; i++) {
-			int j = i+1;
-			list.setText(list.getText()+Integer.toString(j)+". " + HelperSharedPreferences.getSharedPreferencesString(getApplicationContext(),"index_date_"+i, "Not exists")+" / "+ HelperSharedPreferences.getSharedPreferencesString(getApplicationContext(),"index_subject_"+i, "Not exists")+"\n"+ HelperSharedPreferences.getSharedPreferencesString(getApplicationContext(),"index_task_"+i, "Not exists")+"\n"+"\n");
-		}
-		if(list.getText().equals("")) {
-			Toast.makeText(getApplicationContext(), "You don't have any tasks...", Toast.LENGTH_SHORT).show();
-		}
-	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
